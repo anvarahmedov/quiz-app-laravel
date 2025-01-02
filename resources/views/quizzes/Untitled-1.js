39bc220308e66a80ec1ae157feed77f0d5de5fc9
anvarahmedov <boyoftokyo19@gmail.com>
2025-01-03 01:26:59 +0500

    captcha2

--- resources/views/quizzes/Untitled-1.js
+++
@@ -1,9 +0,0 @@
-$res->result = 0;
-auth()
-                        ->user()
-                        ->results()
-                        ->create([
-                            'quiz_id' => $quiz->id,
-                            'user_id' => auth()->user()->id,
-                            'result' => $res->result
-                        ]);
